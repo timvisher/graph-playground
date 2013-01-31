9b9e9f9cd8b05db8362da18f9c84187049f9b4cb
Tim Visher <tvisher@relaynetwork.com>
2013-01-30 21:32:48 -0500

    A little repo to play with prismatic's graph. EOM

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to graph-playground
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ src/graph_playground/core.clj
@@ -0,0 +1,7 @@
+(ns graph-playground.core
+  (:require [plumbing.graph :as graph]))
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
